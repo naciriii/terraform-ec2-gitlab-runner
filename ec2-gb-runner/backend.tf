a6125c3f9aec3e382df780b225c148100556b18b
naciriii <nnacir1938@hotmail.com>
2020-06-29 13:44:48 +0100

    Lint files

--- ec2-gb-runner/backend.tf
+++ ec2-gb-runner/backend.tf
@@ -1,17 +1,17 @@
 terraform {
   backend "s3" {
-    bucket = var.tf-bucket
-    key    = var.tf-state-file
+    bucket  = var.tf-bucket
+    key     = var.tf-state-file
     profile = var.profile
-    region = var.region
+    region  = var.region
   }
 }
 data "terraform_remote_state" "gitlab-runner" {
   backend = "s3"
   config = {
-    bucket = var.tf-bucket
-    key    = var.tf-state-file
+    bucket  = var.tf-bucket
+    key     = var.tf-state-file
     profile = var.profile
-    region = var.region
+    region  = var.region
   }
 }
