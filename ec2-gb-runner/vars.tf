690ddb54ac5205cda0550abddce26d286f4fb79b
naciriii <nnacir1938@hotmail.com>
2020-06-29 13:18:06 +0100

    Add s3 backend and remote state

--- ec2-gb-runner/vars.tf
+++ ec2-gb-runner/vars.tf
@@ -6,6 +6,14 @@ variable "region" {
   description = "AWS region"
   default     = "us-east-1"
 }
+variable "tf-bucket" {
+  description = "Terraform s3 bucket"
+  default     = "tf-gitlab-ci-runner"
+}
+variable "tf-state-file" {
+  description = "Terraform s3 remote state file"
+  default     = "terraform.tfstate"
+}
 variable "gitlab_token" {
   description = "Gitlab ci token"
   default     = "testtest"
